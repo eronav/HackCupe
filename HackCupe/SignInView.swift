6c9cc2929351d45ea93c7646ef6c3faa3455d2b0
Advaith <advaith.anands@gmail.com>
2023-04-15 19:47:30 -0700

    Added link to createAccount

--- HackCupe/SignInView.swift
+++ HackCupe/SignInView.swift
@@ -12,47 +12,49 @@ struct SignInView: View {
     @State private var password: String = ""
 
     var body: some View {
-        VStack {
-            Text("CommUnity")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .padding(.bottom, 50)
-
-            TextField("Username", text: $username)
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(8.0)
-                .padding(.bottom, 20)
-
-            SecureField("Password", text: $password)
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(8.0)
-                .padding(.bottom, 20)
-
-            Button(action: {
-                // Handle sign-in action here
-            }) {
-                Text("Sign In")
-                    .frame(minWidth: 0, maxWidth: .infinity)
+        NavigationView {
+            VStack {
+                Text("CommUnity")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.bottom, 50)
+                
+                TextField("Username", text: $username)
                     .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
+                    .background(Color(.systemGray6))
                     .cornerRadius(8.0)
-            }
-
-            Spacer()
-
-            Button(action: {
-                // Navigate to the new account creation screen
-            }) {
-                NavigationLink(destination: CreateAccountView()) {
-                    Text("Create New Account")
-                        .foregroundColor(Color.blue)
+                    .padding(.bottom, 20)
+                
+                SecureField("Password", text: $password)
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8.0)
+                    .padding(.bottom, 20)
+                
+                Button(action: {
+                    // Handle sign-in action here
+                }) {
+                    Text("Sign In")
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8.0)
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    // Navigate to the new account creation screen
+                }) {
+                    NavigationLink(destination: CreateAccountView()) {
+                        Text("Create New Account")
+                            .foregroundColor(Color.blue)
+                    }
                 }
             }
+            .padding(.horizontal, 20)
         }
-        .padding(.horizontal, 20)
     }
 }
 
