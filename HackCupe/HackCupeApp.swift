2f489917aae428680b306501c3bfd40a04ea8605
Ronav <mailronav@gmail.com>
2023-04-15 17:48:11 -0700

    initial commit

---
+++ HackCupe/HackCupeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackCupeApp.swift
+//  HackCupe
+//
+//  Created by Ronav Dholakia on 4/15/23.
+//
+
+import SwiftUI
+
+@main
+struct HackCupeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
